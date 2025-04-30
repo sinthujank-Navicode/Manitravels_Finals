6678d2feb34b8b3e5de91ebc385df5519629e2fb
sksinthu <sksinthu15@gmail.com>
2025-04-30 17:24:51 +0530

    config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // ✅ enables static export
+
 };
 
 export default nextConfig;
