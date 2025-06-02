df187579f4eb4b0ef2a447cadabf82026c905e05
Kobikan06 <kopyt.navicode@gmail.com>
2025-06-02 09:22:29 +0530

    First version pushed

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div id="home" className="relative overflow-hidden md:mb-0 mb-7">
         <Hero />
       </div>
-      <div id="service " className="m-2 md:m-0 md:mt-12 ">
+      <div id="service " className="m-2 md:m-0 md:mt-1 ">
         <OurService />
       </div>
       <div id="AboutUs" className="m-2 md:m-0 rounded-lg bg-white relative -mb-8">
